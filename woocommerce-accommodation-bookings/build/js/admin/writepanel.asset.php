7b9f657704af6d6898c1929dea5c8ab61544dd68
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 05:00:50 +0000

    1.3.7

--- woocommerce-accommodation-bookings/build/js/admin/writepanel.asset.php
+++ woocommerce-accommodation-bookings/build/js/admin/writepanel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'abd62d8ee0f786b781cd');
+<?php return array('dependencies' => array('jquery'), 'version' => 'a5b6b64af3c8c380c684');
